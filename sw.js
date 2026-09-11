13cb2df2ef0cee98c59a746cd89ecddd9e3cc8bf
cadi81 <cadi81@msn.com>
2026-09-11 15:24:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hausbau-4c169a34cc3c';
+const CACHE='hausbau-fab21fb3ebb8';
 
 const ASSETS=['./','./index.html','./icon.svg','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
